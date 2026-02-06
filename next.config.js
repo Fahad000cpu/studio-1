567a8baae974f0984526971c3e4e3f30550e2cf1
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-06 21:26:44 +0000

    1 of 1 error Next.js (14.2.35) is outdated (learn more)
    Unhandled Runti

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
