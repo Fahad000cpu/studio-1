ccbfe0de6615b20940dcb3b75a43b0deefa30f02
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-06 04:37:23 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-
 import type {NextConfig} from 'next';
+import withSerwistInit from "@serwist/next";
 
-const { default: withSerwist } = require("@serwist/next");
+const withSerwist = withSerwistInit.default;
 
 const nextConfig: NextConfig = {
   /* config options here */
