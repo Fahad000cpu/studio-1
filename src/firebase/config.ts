7bf1d7f8c10e89139c8c0fdb6c1f183648629e17
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-09 05:44:33 +0000

    AIzaSyDX7a6xX71Ju2Y8WOuyD8RwoseKEf

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,8 @@ export const firebaseConfig = {
   "projectId": "studio-6505166944-ae18f",
   "appId": "1:954303139735:web:1cb50131d512627c9d3ed2",
   "storageBucket": "studio-6505166944-ae18f.appspot.com",
-  "apiKey": "AIzaSyB8ADt_BHfhhcIwDax82s13GVYJAefjA0g",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-6505166944-ae18f.firebaseapp.com",
   "measurementId": "G-1TC3B2RSWT",
   "messagingSenderId": "954303139735"
 };
-
-    
