8bbe886d5972ea633e29be14b666007572d72f7e
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-09 09:18:22 +0000

    Config check

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
   "projectId": "studio-6505166944-ae18f",
-  "appId": "1:954303139735:web:1cb50131d512627c9d3ed2",
-  "storageBucket": "studio-6505166944-ae18f.appspot.com",
+  "appId": "1:954303139735:web:d06fadc26caecc199d3ed2",
+  "storageBucket": "studio-6505166944-ae18f.firebasestorage.app",
   "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-6505166944-ae18f.firebaseapp.com",
-  "measurementId": "G-1TC3B2RSWT",
+  "measurementId": "G-VF0N789N79",
   "messagingSenderId": "954303139735"
 };
