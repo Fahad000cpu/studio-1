4840aa1b178bcad2e90f96d0fbf254369c58db7f
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-11 18:44:30 +0000

    Product section me padding hatado right and left of product, kyuki vo pa

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -117,7 +117,7 @@ export default function MainLayout({
               <ThemeToggle />
               <UserNav />
               </header>
-              <main className="flex-1 p-4 md:p-6">
+              <main className="flex-1 py-4 md:py-6">
               {children}
               </main>
           </SidebarInset>
