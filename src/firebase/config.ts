d7817feed604bd7c1d67f0c8ebcdf000a03b0a51
Fahad Khan <newpwafk15520041@gmail.com>
2026-02-16 06:28:39 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,9 @@
 
 export const firebaseConfig = {
-  // IMPORTANT: REPLACE THIS WITH YOUR NEW, WORKING FIREBASE PROJECT CONFIG
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "your-new-project-id.firebaseapp.com",
-  projectId: "your-new-project-id",
-  storageBucket: "your-new-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
-  measurementId: "your-measurement-id"
+  authDomain: "studio-2229476459-4c32f.firebaseapp.com",
+  projectId: "studio-2229476459-4c32f",
+  storageBucket: "studio-2229476459-4c32f.appspot.com",
+  messagingSenderId: "778220308996",
+  appId: "1:778220308996:web:40a6c366fb14d5c7538351"
 };
